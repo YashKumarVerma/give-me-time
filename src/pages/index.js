a7e5ff099e2782d74463c48523f5ad4af441be50
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-06-18 20:16:37 +0530

    chore: structure page, add localStorage item

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import * as React from "react"
+import { useState, useEffect } from "react"
 
+/** load components */
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+/** load logic components   */
 import { Clock } from "../components/clock"
+import { useLocalStorage } from "../hooks/useLocalStorage"
 
 const IndexPage = () => (
   <Layout>
